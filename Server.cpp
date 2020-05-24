8394e4888a17634ccf24155b5e7df7b1353f6d82
Xie Li <lixie15@mails.ucas.ac.cn>
2020-05-25 01:49:57 +0800

    framework impl finished

--- Server.cpp
+++ Server.cpp
@@ -9,7 +9,7 @@ void udp_startRecv_askSnd_answerRecv_responseSnd_handle(int fd) {
 	socklen_t len;
 	int count;
 	struct sockaddr_in gateway_addr;
-	// use for remember the otherside
+	// use for remembering the otherside
 	memset(buf, 0, 1024);
 	len = sizeof(gateway_addr);
 	// recv start part
@@ -18,15 +18,22 @@ void udp_startRecv_askSnd_answerRecv_responseSnd_handle(int fd) {
 		std::cout << "recv data failed" << std::endl;
 		return;
 	}
+	auth_start_packet* asp = (auth_start_packet*)buf;
+	if(asp->auth_hdr.type == 0x1){
+		cout << "Server received start packet" << endl;
+	}
 	std::cout << buf << endl;
-	std::cout << "Reiceive gateway start: " << buf << std::endl;
 	// send ask part
-	// TODO: add 
+	auth_ask_packet aap;
+	aap.auth_hdr.type = 0x2;
+	aap.client_id = asp->client_id;
+	aap.server_id = 0x8;
+	aap.random_num_encrypted = htonl(4096);
 	memset(buf, 0, 1024);
+	memcpy(buf, &aap, sizeof(aap));
 	sendto(fd, buf, 1024, 0, (struct sockaddr*) & gateway_addr, len);
 	std::cout << "Send gateway ask: " << buf << std::endl;
 	// recv answer part
-	// TODO: add
 	memset(buf, 0, 1024);
 	count = recvfrom(fd, buf, 1024, 0, (struct sockaddr*) & gateway_addr, &len);
 	if (count == -1) {
@@ -34,12 +41,21 @@ void udp_startRecv_askSnd_answerRecv_responseSnd_handle(int fd) {
 		return;
 	}
 	std::cout << "Receive gateway answer: " << buf << std::endl;
-
+	auth_answer_packet* aAnsP = (auth_answer_packet*)buf;
+	if(aAnsP->auth_hdr.type == 0x3){
+		cout << "Server receive answer packet" << endl;
+	}
+	identifier client_id = aAnsP->client_id;
 	// send response part
-	// TODO: add
 	memset(buf, 0, 1024);
+	auth_response_packet arep;
+	arep.auth_hdr.type = 0x5;
+	arep.client_id = client_id;
+	arep.client_ip_private_key = 0;
+	arep.server_id = 0x8;
+	memcpy(buf, &arep, sizeof(arep));
 	sendto(fd, buf, 1024, 0, (struct sockaddr*) & gateway_addr, len);
-	std::cout << "Send gateway response: " << buf << std::endl;
+	std::cout << "Send gateway response: Connection built" << buf << std::endl;
 	// connection build
 }
 
